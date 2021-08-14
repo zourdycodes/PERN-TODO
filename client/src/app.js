93a5353614bba9d3a3f499e8f20e67d0ac9268e9
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-14 12:35:24 +0700

    cleaning up the environment + boilerplate

--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +20,4 @@ function App() {
       </header>
     </div>
   );
-}
-
-export default App;
+};
